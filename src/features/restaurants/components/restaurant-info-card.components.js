caa087d0849fdb94c571641a19d3233350fd7752
nigeltavaguta <nigeltavaguta@gmail.com>
2025-06-06 02:39:02 +0200

    modified info card

--- src/features/restaurants/components/restaurant-info-card.components.js
+++ src/features/restaurants/components/restaurant-info-card.components.js
@@ -2,9 +2,11 @@ import React from "react";
 import styled from "styled-components/native";
 import { Text, View } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
 
 const RestaurantCard = styled(Card)`
-    ckground-color: ${(props) => props.theme.colors.bg.primary}; 
+    background-color: ${(props) => props.theme.colors.bg.primary}; 
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
@@ -13,9 +15,26 @@ const RestaurantCardCover = styled(Card.Cover)`
 `;
 
 const Title = styled(Text)`
-    font-family: ${(props) => props.theme.fonts.body};
-    padding: ${(props) => props.theme.space[3]};
+    font-family: ${(props) => props.theme.fonts.heading};
+    font-size: ${(props) => props.theme.fontSizes.title};
+    font-weight: ${(props) => props.theme.fontWeights.bold};
     color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Address = styled(Text)`
+    font-family: ${(props) => props.theme.fonts.body};
+    font-size: ${(props) => props.theme.fontSizes.body};
+
+`;
+
+const Info = styled.View`
+    padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+    flex-direction: row;
+    padding-top: ${(props) => props.theme.space[2]};
+    padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -25,16 +44,24 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         photos = [
             "https://media.istockphoto.com/id/2186643009/photo/woman-in-the-kitchen-preparing-healthy-vegetables-in-plastic-airtight-food-containers.jpg?s=1024x1024&w=is&k=20&c=50hLc4aK79tarZ_SoDa7oHnRYkH9VsAQUtGBlufayEs=",
         ],
-        address = "Hello world",
+        address = "4th Hello world St.",
         isOpenNow = true,
         rating = 4,
         isClosedTemporarily,
     } = restaurant;
-
+    const ratingArray = Array.from(new Array(Math.floor(rating)));
     return (
         <RestaurantCard elevation={5}>
             <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-            <Title>{name}</Title>
-        </RestaurantCard>
+            <Info>
+                <Title>{name}</Title>
+                <Rating>
+                    {ratingArray.map(() => (
+                        <SvgXml xml={star} width={20} height={20} />
+                    ))};
+                </Rating>
+                <Address>{address}</Address>
+            </Info>
+        </RestaurantCard >
     );
 };
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -5,31 +5,35 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import styled from 'styled-components/native';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.components';
 
-
 const isAndroid = Platform.OS === 'android';
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${isAndroid ? `${StatusBar.currentHeight || 0}px` : '0'};
+  ${isAndroid ? `padding-top: ${StatusBar.currentHeight || 24}px` : ''}; // Default to 24px if undefined
+  background-color: #fff; // Ensure solid background
 `;
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
+  background-color: #fff; // Match SafeArea background
 `;
 
 const RestaurantListContainer = styled.View`
   flex: 1;
   padding: ${(props) => props.theme.space[3]};
+  background-color: #fff; // Changed from blue for consistency
 `;
 
-export const RestaurantsScreen = () => (
+export default function RestaurantsScreen() {
+  return (
     <SafeArea>
-        <SearchContainer>
-            <Searchbar placeholder="Search" />
-        </SearchContainer>
-        <RestaurantListContainer>
-            <RestaurantInfoCard />
-        </RestaurantListContainer>
-        <ExpoStatusBar style="auto" />
+      <SearchContainer>
+        <Searchbar placeholder="Search" />
+      </SearchContainer>
+      <RestaurantListContainer>
+        <RestaurantInfoCard />
+      </RestaurantListContainer>
+      <ExpoStatusBar style="auto" backgroundColor="#fff" />
     </SafeArea>
-);
+  );
+}
